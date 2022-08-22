7fbab5f90e95a182aa7e3a578455ae9fbc6badb0
Anastácia Canto <91332251+Anastacia-Canto@users.noreply.github.com>
2022-08-22 13:17:32 +0100

    Update minitalk.h

--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -13,8 +13,9 @@
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include <unistd.h>
 # include <signal.h>
+# include <sys/types.h>
+# include <unistd.h>
 # include "printf/ft_printf.h"
 
 int		ft_atoi(const char *str);
